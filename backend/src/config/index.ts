3848de5b43e0197bcfcd46cd22afd7634400f1de
Hasanthika Gamage <hasanthika@axis-tech.co>
2025-09-07 11:21:55 +0530

    configuration file added

---
+++ backend/src/config/index.ts
@@ -0,0 +1,5 @@
+export const configurations = {
+    productTable: process.env.PRODUCT_TABLE,
+    productQueueUrl: process.env.PRODUCT_QUEUE_URL,
+    sellerId: "sellerId", //@TODO -> seller id should be taken from the token
+};
